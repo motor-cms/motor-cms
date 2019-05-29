6291a8dc44e538a54edc78cc468bffdfc6a2d2a2
Reza Esmaili <github@dfox.info>
2019-05-29 14:46:04 +0200

    Fix proper call

--- resources/views/layouts/frontend/partials/template-sections.blade.php
+++ resources/views/layouts/frontend/partials/template-sections.blade.php
@@ -1,11 +1,11 @@
 @foreach($rows as $row)
     <div class="grid-x grid-margin-x">
         @foreach($row as $element)
-            <div class="cell {{Arr::get($element, 'class')}} medium-{{Arr::get($element, 'width')}}">
-                @if (Arr::get($element, 'items'))
-                    @include('motor-cms::layouts.frontend.partials.template-sections', ['rows' => Arr::get($element, 'items')])
+            <div class="cell {{Illuminate\Support\Arr::get($element, 'class')}} medium-{{Illuminate\Support\Arr::get($element, 'width')}}">
+                @if (Illuminate\Support\Arr::get($element, 'items'))
+                    @include('motor-cms::layouts.frontend.partials.template-sections', ['rows' => Illuminate\Support\Arr::get($element, 'items')])
                 @endif
-                @yield(Arr::get($element, 'container'))
+                @yield(Illuminate\Support\Arr::get($element, 'container'))
             </div>
         @endforeach
     </div>
