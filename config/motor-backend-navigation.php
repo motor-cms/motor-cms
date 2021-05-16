5a4b96ac83d84df2397f02a76292590c7da9c256
Reza Esmaili <bitbucket@dfox.info>
2021-05-16 22:43:39 +0200

    Change navigation routes

--- config/motor-backend-navigation.php
+++ config/motor-backend-navigation.php
@@ -4,15 +4,15 @@ return [
     'items' => [
         800 => [
             'slug'        => 'cms',
-            'name'        => 'motor-cms::backend/global.cms',
-            'icon'        => 'fa fa-home',
+            'name'        => 'motor-cms.global.cms',
+            'icon'        => "font",
             'route'       => null,
             'roles'       => [ 'SuperAdmin' ],
             'permissions' => [ 'cms.read' ],
             'items'       => [
                 100 => [ // <-- !!! replace 542 with your own sort position !!!
                     'slug'        => 'navigation_trees',
-                    'name'        => 'motor-cms::backend/navigation_trees.navigation_trees',
+                    'name'        => 'motor-cms.navigation_trees.navigation_trees',
                     'icon'        => 'fa fa-plus',
                     'route'       => 'backend.navigation_trees.index',
                     'roles'       => [ 'SuperAdmin' ],
@@ -21,7 +21,7 @@ return [
                 ],
                 110 => [ // <-- !!! replace 895 with your own sort position !!!
                     'slug' => 'pages',
-                    'name'  => 'motor-cms::backend/pages.pages',
+                    'name'  => 'motor-cms.pages.pages',
                     'icon'  => 'fa fa-plus',
                     'route' => 'backend.pages.index',
                     'roles'       => [ 'SuperAdmin' ],
