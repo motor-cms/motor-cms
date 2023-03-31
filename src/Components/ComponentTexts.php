7f3cf7853a6f20fc420c6459ef373acd723bf764
Reza Esmaili <bitbucket@dfox.info>
2023-04-01 00:12:46 +0200

    Remove cropping option

--- src/Components/ComponentTexts.php
+++ src/Components/ComponentTexts.php
@@ -48,16 +48,16 @@ class ComponentTexts
 
         if ($image) {
             // If we have a cropped image, load that one from the disk
-            if (Arr::get($image->custom_properties, 'crop')) {
-                $hash = Arr::get($image->custom_properties, 'crop.x1').Arr::get($image->custom_properties, 'crop.x2').Arr::get($image->custom_properties, 'crop.y1').Arr::get($image->custom_properties, 'crop.y2');
-
-                $pathinfo = pathinfo(public_path().$image->file->getFirstMedia('file')->getUrl());
-                $thumb = str_replace(public_path(), '', $pathinfo['dirname']).'/conversions/'.$pathinfo['filename'].'-'.md5($hash).'.jpg';
-                $file = $thumb;
-            } else {
-                $thumb = $image->file->getFirstMedia('file')->getUrl('thumb');
-                $file = $image->file->getFirstMedia('file')->getUrl();
-            }
+            //if (Arr::get($image->custom_properties, 'crop')) {
+            //    $hash = Arr::get($image->custom_properties, 'crop.x1').Arr::get($image->custom_properties, 'crop.x2').Arr::get($image->custom_properties, 'crop.y1').Arr::get($image->custom_properties, 'crop.y2');
+            //
+            //    $pathinfo = pathinfo(public_path().$image->file->getFirstMedia('file')->getUrl());
+            //    $thumb = str_replace(public_path(), '', $pathinfo['dirname']).'/conversions/'.$pathinfo['filename'].'-'.md5($hash).'.jpg';
+            //    $file = $thumb;
+            //} else {
+            $thumb = $image->file->getFirstMedia('file')->getUrl('thumb');
+            $file = $image->file->getFirstMedia('file')->getUrl();
+            //}
 
             $position = Arr::get($image->custom_properties, 'position', 'right');
             $enlarge = Arr::get($image->custom_properties, 'enlarge', false);
