020c61edc0d895c26945fc1678e66f2fa8469ab5
Reza Esmaili <github@dfox.info>
2026-03-14 16:46:15 +0100

    UX audit fixes: accessibility, touch targets, consistency, mobile

--- resources/views/frontend/components/text-tw.blade.php
+++ resources/views/frontend/components/text-tw.blade.php
@@ -1,21 +1,21 @@
-<div class="prose max-w-none">
-    @if($component->headline)<h1>{{ $component->headline }}</h1>@endif
+<div class="prose max-w-none break-words overflow-x-auto">
+    @if($component->headline)<h2>{{ $component->headline }}</h2>@endif
     <div id="{{ $component->anchor }}"></div>
     @if (is_null($file))
         {!! $component->body !!}
     @else
         @if ($position === 'top')
-            <div class="rounded-lg shadow not-prose mb-4">
+            <div class="rounded-lg shadow-[0_4px_12px_rgba(0,0,0,0.4)] not-prose mb-4">
                 <a href="{{ $file }}"><img src="{{ $thumb }}" alt="{{ $description }}" class="rounded-lg"></a>
             </div>
             {!! $component->body !!}
         @elseif($position === 'bottom')
             {!! $component->body !!}
-            <div class="rounded-lg shadow not-prose mt-4">
+            <div class="rounded-lg shadow-[0_4px_12px_rgba(0,0,0,0.4)] not-prose mt-4">
                 <a href="{{ $file }}"><img src="{{ $thumb }}" alt="{{ $description }}" class="rounded-lg"></a>
             </div>
         @else
-            <div class="rounded-lg shadow not-prose float-{{ $position }} @if($position === 'left') mr-4 mb-2 @else ml-4 mb-2 @endif">
+            <div class="rounded-lg shadow-[0_4px_12px_rgba(0,0,0,0.4)] not-prose float-{{ $position }} @if($position === 'left') mr-4 mb-2 @else ml-4 mb-2 @endif">
                 <a href="{{ $file }}"><img src="{{ $thumb }}" alt="{{ $description }}" class="rounded-lg"></a>
             </div>
             {!! $component->body !!}
