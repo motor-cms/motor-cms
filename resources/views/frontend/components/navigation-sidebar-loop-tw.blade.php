73c7c0b31249f17520d7427195fe00ec95dbb7b6
Reza Esmaili <github@dfox.info>
2026-03-14 08:52:26 +0100

    Rewrite navigation components: plain Tailwind, remove daisyUI menu classes

--- resources/views/frontend/components/navigation-sidebar-loop-tw.blade.php
+++ resources/views/frontend/components/navigation-sidebar-loop-tw.blade.php
@@ -1,14 +1,23 @@
-@if ($navigationItem->hasChildren())
-    <ul class="menu menu-vertical @if ($depth > 0) ml-4 @endif">
-        @foreach ($navigationItem->children as $child)
-            @if ($child->is_visible && $child->is_active)
-                <li class="@if (in_array($child->full_slug, $activeNavigationSlugs)) active @endif">
-                    <a href="{{ route('frontend.pages.index', ['slug' => $child->full_slug])}}" class="@if (in_array($child->full_slug, $activeNavigationSlugs)) active @endif">{{$child->name}}</a>
-                    @if (!is_null($navigationItem) && in_array($child->full_slug, $activeNavigationSlugs))
-                        @include('motor-cms::frontend.components.navigation-sidebar-loop-tw', ['navigationItem' => $child, 'depth' => $depth+1])
-                    @endif
-                </li>
-            @endif
-        @endforeach
-    </ul>
-@endif
+<ul class="space-y-0.5 @if($depth > 0) ml-4 @endif">
+    @foreach($items as $item)
+        @if ($item->is_visible && $item->is_active)
+            <li>
+                <a href="{{ route('frontend.pages.index', ['slug' => $item->full_slug]) }}"
+                   class="block px-3 py-1.5 rounded-md text-sm transition-colors
+                          @if(in_array($item->full_slug, $activeNavigationSlugs))
+                              text-heading bg-surface-raised font-medium
+                          @else
+                              text-text hover:text-heading hover:bg-surface-raised
+                          @endif">
+                    {{ $item->name }}
+                </a>
+                @if($item->children && $item->children->count() > 0)
+                    @include('motor-cms::frontend.components.navigation-sidebar-loop-tw', [
+                        'items' => $item->children,
+                        'depth' => $depth + 1
+                    ])
+                @endif
+            </li>
+        @endif
+    @endforeach
+</ul>
--- resources/views/frontend/components/navigation-sidebar-tw.blade.php
+++ resources/views/frontend/components/navigation-sidebar-tw.blade.php
@@ -1,1 +1,6 @@
-@include('motor-cms::frontend.components.navigation-sidebar-loop-tw', ['navigationItem' => $activeTopLevelNavigationItem, 'depth' => 0])
+<nav class="space-y-2">
+    @include('motor-cms::frontend.components.navigation-sidebar-loop-tw', [
+        'items' => $activeTopLevelNavigationItem->children,
+        'depth' => 0
+    ])
+</nav>
